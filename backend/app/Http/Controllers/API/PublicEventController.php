93059d8e9c18e3df27f47a389b86af766ccac3b5
Pranav <prasharpranav@gmail.com>
2025-12-03 23:30:48 +0530

    Test deployment v5.1 - Testing updated deployment workflow changes

--- backend/app/Http/Controllers/API/PublicEventController.php
+++ backend/app/Http/Controllers/API/PublicEventController.php
@@ -11,7 +11,7 @@ class PublicEventController extends Controller
 {
     /**
      * Get all active events for public display
-     * Updated: 2025-12-03 - Deployment test version 5.0 - Fixed Laravel deployment (proper cache exclusion)
+     * Updated: 2025-12-03 - Deployment test version 5.1 - Testing updated deployment workflow
      */
     public function index(Request $request): JsonResponse
     {
