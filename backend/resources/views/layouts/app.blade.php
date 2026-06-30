f5d2ec384331b6a9a24adcbcae25afee07e42d47
pranav <prasharpranav@gmail.com>
2026-07-01 02:22:45 +0530

    Update backend favicon to use fav.ico

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <link rel="icon" href="{{ asset('images/favicon.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('images/fav.ico') }}" type="image/x-icon">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
