85ad53fc018d5c4e6646841aa46cbffa6bfc63d8
pranav <prasharpranav@gmail.com>
2026-07-01 02:36:17 +0530

    Add new admin user yusri@thefacecraft.com to seeder

--- backend/database/seeders/AdminUserSeeder.php
+++ backend/database/seeders/AdminUserSeeder.php
@@ -11,11 +11,24 @@ class AdminUserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@kuantan188.com',
-            'password' => Hash::make('password123'),
-            'email_verified_at' => now()
-        ]);
+        // Create default admin user
+        User::firstOrCreate(
+            ['email' => 'admin@kuantan188.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('password123'),
+                'email_verified_at' => now()
+            ]
+        );
+
+        // Create Yusri admin user
+        User::firstOrCreate(
+            ['email' => 'yusri@thefacecraft.com'],
+            [
+                'name' => 'Yusri - TheFacecraft',
+                'password' => Hash::make('Ysr@TFC2026!#SecureAdmin'),
+                'email_verified_at' => now()
+            ]
+        );
     }
 }
